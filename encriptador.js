565761c58e6e4ce574c67127631ef36853b4391e
alirio96 <137359013+alirio96@users.noreply.github.com>
2024-07-09 04:38:35 -0500

    comits encriptador

---
+++ encriptador.js
@@ -0,0 +1,8 @@
+/* const ingreseTexto=document.getElementById("ingreseTexto");
+const botonEncriptar=document.getElementById("botonEncriptar");
+const botonDesencriptar=document.getElementById("botonDesencriptar");
+const botonCopiar=document.getElementById("botonCopiar");
+const mensajeFinal=document.getElementById("mensajeFinal");*/
+
+
+console-console.log((ingrese_texto.value));
